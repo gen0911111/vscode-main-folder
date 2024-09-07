331836ccdbc01a6280be6c88c45d56e2f1f62784
gen0911111 <genm1109@gmail.com>
2024-09-07 12:58:04 +0800

    commit

---
+++ HTML, CSS, JS/Music Player App/script.js
@@ -0,0 +1,3 @@
+var audio = document.getElementById("player");
+var playlist = document.querySelectorAll("list");
+var tracks = ["HTML, CSS, JS\Music Player App\Bruno Mars - Just the Way You Are.mp3", "HTML, CSS, JS\Music Player App\Bruno Mars - Leave the Door Open.mp3", "HTML, CSS, JS\Music Player App\Bruno Mars - Locked Out Of Heaven.mp3"];
