d971d1293dc3dcab489943bdc104b85f2c53b753
gen0911111 <genm1109@gmail.com>
2024-10-16 19:30:51 +0800

    commit

--- Python/eggcatch/catapiler.py
+++ Python/eggcatch/catapiler.py
@@ -33,3 +33,47 @@ def scoreDisplay(score):
     y = (t.window_height()/2)-50
     scoreTurtle.setpos(x, y)
     scoreTurtle.write(str(score), align="right", font=("Arial", 12))
+
+def spawnLeaf():
+    leaf.hideturtle()
+    leaf.setx(r.randint(-200, 200))
+    leaf.sety(r.randint(-200, 200))
+    leaf.showturtle()
+
+def hitWall():
+    rightWall = t.window_width()/2
+    leftWall = -t.window_width()/2
+    upWall = t.window_height()/2
+    downWall = -t.window_height()/2
+    (x,y) = catapiler.pos()
+    outOfBoundary = x > rightWall or x < leftWall or y > upWall or y < downWall
+    return outOfBoundary
+
+def gameover():
+    catapiler.color('red')
+    leaf.color('black')
+    t.penup()
+    t.hideturtle()
+    t.write("Game Over", align="center", font=("Arial", 20))
+
+def gameLogic():
+    global gameIsStarted
+    if gameIsStarted:
+        return
+    gameIsStarted = True
+
+def up():
+    if catapiler.heading() == 0 or catapiler.heading() == 180:
+        catapiler.setheading(90)
+
+def down():
+    if catapiler.heading() == 0 or catapiler.heading() == 180:
+        catapiler.setheading(270)
+
+def left():
+    if catapiler.heading() == 90 or catapiler.heading() == 270:
+        catapiler.setheading(180)
+
+def right():
+    if catapiler.heading() == 90 or catapiler.heading() == 270:
+        catapiler.setheading(0)
