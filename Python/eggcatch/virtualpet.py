d3c9435428ebb786bcf45bb1655d1a0bd47d2db0
gen0911111 <genm1109@gmail.com>
2024-10-26 13:01:17 +0800

    commit

--- Python/eggcatch/virtualpet.py
+++ Python/eggcatch/virtualpet.py
@@ -10,7 +10,7 @@ body = ds.create_oval(20, 20, 300, 250, outline="black", fill=ds.bodycolor)
 eyeLeft = ds.create_oval(30, 200, 60, 230, outline="black", fill="black")
 eyeRight = ds.create_oval(260, 200, 290, 230, outline="black", fill="black")
 mouth = ds.create_line(100, 60, 220, 60, outline="black", fill="black")
-
+ds.pack()
 
 
 
