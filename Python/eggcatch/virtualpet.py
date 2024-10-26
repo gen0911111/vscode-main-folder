2d6c0f43bac949edfbf7a56e6e79da8df62b6a66
gen0911111 <genm1109@gmail.com>
2024-10-26 12:15:56 +0800

    commit

---
+++ Python/eggcatch/virtualpet.py
@@ -0,0 +1,17 @@
+from tkinter import Tk, Canvas, HIDDEN, NORMAL
+
+root = Tk()
+root.title("Virtual Pet Blob")
+ds = Canvas(root, width=400, height=400)
+ds.configure(bg="orange", highlightthickness=0)
+ds.bodycolor="lightblue"
+
+body = ds.create_oval(20, 20, 300, 250, outline="black", fill=ds.bodycolor)
+eyeLeft = ds.create_oval(30, 200, 60, 230, outline="black", fill="black")
+eyeRight = ds.create_oval(260, 200, 290, 230, outline="black", fill="black")
+mouth = ds.create_line(100, 60, 220, 60, outline="black", fill="black")
+
+
+
+
+root.mainloop()
